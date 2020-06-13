b437c60d15f793c9028bd882c93267266efeb2cd
Reeka <reekamaharaj@gmail.com>
2020-06-13 15:41:45 -0500

    app working, minimallllyyyyyy

---
+++ client/src/pages/Questions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Questions() {
+    return <h1>Questions Loaded</h1>
+}
+
+export default Questions;
---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    // Gets all questions
+    getQuestions: function () {
+        return axios.get("/api/questions");
+    },
+    // Gets the question with the given id
+    getQuestion: function (id) {
+        return axios.get("/api/questions/" + id);
+    },
+    // Deletes the question with the given id
+    deleteQuestion: function (id) {
+        return axios.delete("/api/questions/" + id);
+    },
+    // Saves a question to the database
+    saveQuestion: function (questionData) {
+        return axios.post("/api/questions", questionData);
+    },
+};
