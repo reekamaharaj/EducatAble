f7b57772098e0a6ca4bb66f34a4022c86742fd86
vivgarcia <viv.garcia88@gmail.com>
2020-06-19 18:10:48 -0500

    fixing routing issue

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 export default function Section() {
+  
     return (
         <div style={{ width: '100%'}}>
+            <Box display='flex'>
+                <h2>Did you know?</h2>
+            </Box>
             <Box display='flex'>
                 <p>Content</p>
             </Box>
