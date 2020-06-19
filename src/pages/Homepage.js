901bab4af6fbf7bf0c7ec4fc4718e58cf4174c84
vivgarcia <viv.garcia88@gmail.com>
2020-06-18 23:16:15 -0500

    adding content sections

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,7 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import Logo from '../components/Logo/index';
 import LearnMore from '../components/LearnMore/index';
-
+import Section from '../components/Section/index';
 const useStyles = makeStyles({
     root: {
         backgroundColor: '#72A0C1',
@@ -13,10 +13,16 @@ const useStyles = makeStyles({
 
 function Homepage() {
     const classes = useStyles();
-   return <Container className={classes.root}>
-                <Logo />
-                <LearnMore />
+   return <div style={{ width: '100%'}}>
+        <Container className={classes.root}>
+            <Logo />
+            <LearnMore />
         </Container>
+        <Container>
+            <Section />
+            <Section />
+        </Container>
+        </div>
 }
 
 export default Homepage;
