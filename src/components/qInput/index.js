c9549ca3a056506dbdea851e28f81c7ab044fe71
vivgarcia <viv.garcia88@gmail.com>
2020-06-29 13:41:39 -0500

    adding question for logged in view

---
+++ src/components/qInput/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+export default function qInput() {
+    return (
+        <div>
+            <form>
+               <p>CONTENT</p>
+            </form>
+        </div>
+    )
+}
