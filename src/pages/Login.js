efef2e798483505328934ec9ab3a045ad5c6b8c9
claire-labry <clairestjlabry@gmail.com>
2020-06-30 18:56:28 -0500

    changing colors to be more uniform

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,111 +1,107 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { TextField, Button, Card } from "@material-ui/core";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { TextField, Button, Card } from '@material-ui/core';
 
 function Login() {
-    const [isError, setIsError] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [resText, setResText] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [resText, setResText] = useState('');
 
-    function postLogin(){
-        axios
-        .post("/auth/login", {
-            email,
-            password
-        })
-        .then((result) => { 
-            const data = result.data;
-            if (result.status === 200) {
-                setIsError(false);
-                console.log(data);
-                return setResText(data);
-            } else {
-                setIsError(true);
-                return setResText("Username or password was incorrect");
-            }
-        });
+  function postLogin() {
+    axios
+      .post('/auth/login', {
+        email,
+        password,
+      })
+      .then((result) => {
+        const data = result.data;
+        if (result.status === 200) {
+          setIsError(false);
+          console.log(data);
+          return setResText(data);
+        } else {
+          setIsError(true);
+          return setResText('Username or password was incorrect');
         }
+      });
+  }
 
-    return (
-        <React.Fragment>
-            <br />
-                <Card style={styles.card}>
-                    <Button style={styles.button}>{resText}</Button>
-                    <br />
-                    <TextField
-                        style={styles.text}
-                        id="standard-basic"
-                        label="Enter Your Email"
-                        variant="outlined"
-                        type="username"
-                        value={email}
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                        placeholder="email"
-                    />
-                    <br />
-                    <TextField
-                        style={styles.text}
-                        id="filled-password-input"
-                        variant="outlined"
-                        label="Enter Your Password"
-                        type="password"
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                        placeholder="password"
-                    />
-                    <br />
-                    <Button
-                        variant="contained"
-                        onClick={postLogin}
-                        style={styles.button}>
-                        Log-in
-                    </Button>
-                    <br />
-                    <Button style={styles.button}>
-                        <Link to="/register" style={styles.link}>
-                            Don't have an account?
-                        </Link>
-                    </Button>
-                </Card>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <br />
+      <Card style={styles.card}>
+        <Button style={styles.button}>{resText}</Button>
+        <br />
+        <TextField
+          style={styles.text}
+          id='standard-basic'
+          label='Enter Your Email'
+          variant='outlined'
+          type='username'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder='email'
+        />
+        <br />
+        <TextField
+          style={styles.text}
+          id='filled-password-input'
+          variant='outlined'
+          label='Enter Your Password'
+          type='password'
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder='password'
+        />
+        <br />
+        <Button variant='contained' onClick={postLogin} style={styles.button}>
+          Log-in
+        </Button>
+        <br />
+        <Button style={styles.button}>
+          <Link to='/register' style={styles.link}>
+            Don't have an account?
+          </Link>
+        </Button>
+      </Card>
+    </React.Fragment>
+  );
 }
 
 const styles = {
-    button: {
-        margin: 10,
-        background: "linear-gradient(45deg, #254E58 30%, #88BDBC 90%)",
-        border: 0,
-        borderRadius: 3,
-        color: "white",
-        height: 48,
-        padding: "0 30px",
-    },
+  button: {
+    margin: 10,
+    background: '#72A0C1',
+    border: 0,
+    borderRadius: 3,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
 
-    card: {
-        boxSizing: "border-box",
-        maxWidth: "300px",
-        margin: "auto",
-        textAlign: "center",
-    },
+  card: {
+    boxSizing: 'border-box',
+    maxWidth: '300px',
+    margin: 'auto',
+    textAlign: 'center',
+  },
 
-    text: {
-        margin: 15,
-    },
-    link: {
-        textDecoration: "none",
-        color: "white",
-    },
+  text: {
+    margin: 15,
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white',
+  },
 };
 
 export default Login;
-
 
 // {setIsError === true ? (
 //     <Card style={styles.card}>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,110 +1,110 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { TextField, Button, Card } from "@material-ui/core";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { TextField, Button, Card } from '@material-ui/core';
 
 function Register() {
-    const [isError, setIsError] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [resText, setResText] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [resText, setResText] = useState('');
 
-    function postRegister(){
-        axios
-        .post("/auth/register", {
-            email,
-            password
-        })
-        .then((result) => {
-            const data = result.data;
-            if (result.status === 200) {
-                setIsError(false);
-                return setResText(data);
-            } else {
-                setIsError(true);
-                setResText("Error");
-            }
-        });
-    }
-    return (
-        <React.Fragment>
-            <div>
-                <br />
-                <Card style={styles.card}>
-                <Button style={styles.button}>{resText}</Button>
-                    <br />
-                    <TextField
-                        style={styles.text}
-                        id="standard-basic"
-                        label="Enter Your Email"
-                        variant="outlined"
-                        type="username"
-                        value={email}
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                        placeholder="email"
-                    />
-                    <br />
-                    <TextField
-                        style={styles.text}
-                        id="filled-password-input"
-                        label="Enter Your Password"
-                        variant="outlined"
-                        type="password"
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                        placeholder="password"
-                    />
-                    <br />
-                    <Button
-                        variant="contained"
-                        primary={"true"}
-                        style={styles.button}
-                        onClick={postRegister}>
-                        Register
-                    </Button>
-                    <br />
-                    <Button style={styles.button}>
-                        <Link to="/login" style={styles.link}>
-                            Already have an account?
-                        </Link>
-                    </Button>
-                </Card>
-            </div>
-        </React.Fragment>
-    );
+  function postRegister() {
+    axios
+      .post('/auth/register', {
+        email,
+        password,
+      })
+      .then((result) => {
+        const data = result.data;
+        if (result.status === 200) {
+          setIsError(false);
+          return setResText(data);
+        } else {
+          setIsError(true);
+          setResText('Error');
+        }
+      });
+  }
+  return (
+    <React.Fragment>
+      <div>
+        <br />
+        <Card style={styles.card}>
+          <Button style={styles.button}>{resText}</Button>
+          <br />
+          <TextField
+            style={styles.text}
+            id='standard-basic'
+            label='Enter Your Email'
+            variant='outlined'
+            type='username'
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder='email'
+          />
+          <br />
+          <TextField
+            style={styles.text}
+            id='filled-password-input'
+            label='Enter Your Password'
+            variant='outlined'
+            type='password'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder='password'
+          />
+          <br />
+          <Button
+            variant='contained'
+            primary={'true'}
+            style={styles.button}
+            onClick={postRegister}
+          >
+            Register
+          </Button>
+          <br />
+          <Button style={styles.button}>
+            <Link to='/login' style={styles.link}>
+              Already have an account?
+            </Link>
+          </Button>
+        </Card>
+      </div>
+    </React.Fragment>
+  );
 }
 const styles = {
-    button: {
-        margin: 10,
-        background: "linear-gradient(45deg, #254E58 30%, #88BDBC 90%)",
-        border: 0,
-        borderRadius: 3,
-        color: "white",
-        height: 48,
-        padding: "0 30px",
-    },
+  button: {
+    margin: 10,
+    background: '#72A0C1',
+    border: 0,
+    borderRadius: 3,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
 
-    card: {
-        boxSizing: "border-box",
-        maxWidth: "300px",
-        margin: "auto",
-        textAlign: "center",
-    },
+  card: {
+    boxSizing: 'border-box',
+    maxWidth: '300px',
+    margin: 'auto',
+    textAlign: 'center',
+  },
 
-    text: {
-        margin: 15,
-    },
-    link: {
-        textDecoration: "none",
-        color: "white",
-    },
+  text: {
+    margin: 15,
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white',
+  },
 };
 export default Register;
-
 
 // {setIsError === true ? (
 //     <Card style={styles.card}>
