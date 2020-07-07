26cca57ecd1452920d9c3feb838263a8d7ac1947
Reeka <reekamaharaj@gmail.com>
2020-07-07 11:09:40 -0500

    still reviewing

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,21 +1,15 @@
 import * as React from 'react';
 import axios from 'axios';
 
-
-
 function Admin() {
-const [newQ, setNewQ] = React.useState('');
-const populateQs = () => {
-    axios
-      .get('/api/newQuestion')
-      .then((res) => setNewQ(res.data))
-      .catch((err) => console.log(err));
-  };
- 
-    return(
-        <div>
-            
-        </div>
-    )
+    const [newQ, setNewQ] = React.useState('');
+    const populateQs = () => {
+        axios
+            .get('/api/newQuestion')
+            .then((res) => setNewQ(res.data))
+            .catch((err) => console.log(err));
+    };
+
+    return <div></div>;
 }
 export default Admin;
